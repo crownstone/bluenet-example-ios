8a544506eff8c27caf44e0f6cfb8eff61f22173d
Alex de Mulder <alexdemulder@gmail.com>
2016-04-11 15:45:26 +0200

    Initial Commit

---
+++ Bluenet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bluenet
+//
+//  Created by Alex de Mulder on 11/04/16.
+//  Copyright © 2016 Alex de Mulder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
